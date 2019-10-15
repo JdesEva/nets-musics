e3bd70839922d28c74bbb0708f6edb680097a82c
Jdeshz <17858801330@163.com>
2019-10-15 17:22:38 +0800

    暂时性修复拖拽问题

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ let mainWindow
 function createWindow() {
   //创建浏览器窗口,宽高自定义具体大小你开心就好
   mainWindow = new BrowserWindow({ width: 1024, height: 678, frame: false })
-
   /*
    * 加载应用-----  electron-quick-start中默认的加载入口 */
   mainWindow.loadURL(
--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 const Header: React.FC<any> = (props: any) => {
-  return <div className="nets-header">toubu</div>
+  return (
+    <div className="nets-header">
+      <div className="nets-logo">
+        <p>
+          <a href="/#">网易云音乐</a>
+        </p>
+      </div>
+      <div className="nets-pagination">
+        <input type="text" />
+      </div>
+    </div>
+  )
 }
 export default Header
