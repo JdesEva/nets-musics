8c2d35e7bb09020a1ec93ce8280d2a75f7a08d74
Jdeshz <17858801330@163.com>
2019-10-16 10:51:12 +0800

    备份

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -9,9 +9,18 @@ const Header: React.FC<any> = (props: any) => {
         </p>
       </div>
       <div className="nets-pagination">
+        <span onClick={() => console.log(1)}>
+          <i className="icon-goLeft1 iconfont"></i>
+        </span>
+        <span onClick={() => console.log(2)}>
+          <i className="icon-goLeft iconfont"></i>
+        </span>
+      </div>
+      <div className="nets-search">
         <input type="text" />
       </div>
     </div>
   )
 }
+
 export default Header
