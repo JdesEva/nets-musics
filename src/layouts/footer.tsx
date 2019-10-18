57b8a9a64d2d057f62736ee7601a2238e4bf311e
Jdeshz <17858801330@163.com>
2019-10-18 17:21:49 +0800

    音量调节功能

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
 import { Slider } from 'antd'
 
+interface Voice {
+  nowVolumn: number
+  oldVolumn: number
+}
+
 const Footer: React.FC<any> = props => {
-  const [play, setPlay] = useState<boolean>(false)
-  const [voice, setVoice] = useState<boolean>(false)
+  const [play, setPlay] = useState<boolean>(false) // 播放图标
+  const [volumn, setVolumn] = useState<Voice>({ nowVolumn: 0, oldVolumn: 48 }) // 音量绑定值 - 应当设置初始值为系统音量即可 第一个值永远都是改变的后的值，第二个值是改变之前的值
+  const [process, setProcess] = useState<number>(0) // 播放进度
 
   /**
    * 点击播放按钮的操作
@@ -13,10 +19,37 @@ const Footer: React.FC<any> = props => {
   }
 
   /**
-   * 静音
+   * 播放进度
+   * @param val {number} 播放进度百分比
    */
-  const _toggleVoice = (): void => {
-    setVoice(!voice)
+  const _onchangeProcess = (val: number): void => {
+    setProcess(val)
+  }
+
+  /**
+   * 调整音量
+   * @param val {number} 音量大小
+   */
+  const _onchangeVolumn = (val: number): void => {
+    setVolumn({ ...volumn, nowVolumn: val })
+  }
+
+  /**
+   * 调整音量，保证静音之后的正确
+   * @param val {number} 音量大小
+   */
+  const _onchangeVolumnSync = (val: number): void => {
+    if (val !== 0) setVolumn({ ...volumn, oldVolumn: val })
+  }
+
+  /**
+   * 静音按钮操作
+   */
+  const _toggleNoVoice = (): void => {
+    setVolumn({
+      ...volumn,
+      nowVolumn: volumn.nowVolumn === 0 ? volumn.oldVolumn : 0
+    })
   }
 
   return (
@@ -37,17 +70,26 @@ const Footer: React.FC<any> = props => {
         </p>
       </div>
       <div className="nets-process">
-        <Slider tooltipVisible={false}></Slider>
+        <Slider
+          value={process}
+          onChange={(val: any) => _onchangeProcess(val)}
+          tooltipVisible={false}
+        ></Slider>
       </div>
       <div className="nets-voice">
-        <div onClick={_toggleVoice}>
-          {voice ? (
+        <div onClick={_toggleNoVoice}>
+          {volumn.nowVolumn === 0 ? (
             <i className="iconfont icon-jingyin"></i>
           ) : (
             <i className="iconfont icon-shengyin"></i>
           )}
         </div>
-        <Slider tooltipVisible={false}></Slider>
+        <Slider
+          value={volumn.nowVolumn}
+          onChange={(val: any) => _onchangeVolumn(val)}
+          onAfterChange={(val: any) => _onchangeVolumnSync(val)}
+          tooltipVisible={false}
+        ></Slider>
       </div>
       <div className="nets-bar">1</div>
     </div>
